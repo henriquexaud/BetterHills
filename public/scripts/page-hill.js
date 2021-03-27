435a884a690f1353cef2a44efd0a84514f868ecf
Henrique Xaud <xxhenriquexaud@gmail.com>
2021-03-27 14:33:06 -0400

    first commit

---
+++ public/scripts/page-hill.js
@@ -0,0 +1,60 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false
+
+}
+
+
+// create map
+const map = L.map('mapid', options).setView([2.838165, -60.652934], 14);
+
+// create and add titleLayer and copyRight
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+// create icon
+const icon = L.icon({
+    iconUrl: "./public/images/marker-icon.png",
+    iconSize: [68, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+
+})
+
+const popup1 = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Ladeira da Getúlio <a href = "hill.html?id=1" class= "choose-hill" ><img src="./public/images/arrow-white.svg"></a>')
+
+// create and add marker
+L
+    .marker([2.838165, -60.652934], { icon })
+    .addTo(map);
+
+// image gallery
+
+function selectImage(event) {
+    const button = event.currentTarget
+
+    //remover todas as classes ativas
+    const buttons = document.querySelectorAll(".images button")
+    buttons.forEach(removeActiveClass)
+
+    function removeActiveClass(button) {
+        button.classList.remove("active")
+    }
+
+    //selecionar a imagem clicada
+    const image = button.children[0]
+    const imageContainer = document.querySelector(".hill-details > img")
+
+    //atualizar o conteiner de imagem
+    imageContainer.src = image.src
+
+    //adicionar a classe .active para o botão clicado
+    button.classList.add('active')
+}
---
+++ public/scripts/page-hills.js
@@ -0,0 +1,91 @@
+// create map
+const map = L.map('mapid').setView([2.8221589, -60.673142], 14);
+
+// create and add titleLayer and copyRight
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// create icon
+const icon = L.icon({
+    iconUrl: "./public/images/marker-icon.png",
+    iconSize: [68, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+
+})
+
+// create popup overlay
+const popup1 = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Ladeira da Getúlio <a href = "hill.html?id=1" class= "choose-hill" ><img src="./public/images/arrow-white.svg"></a>')
+
+const popup2 = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Pista do Anauá <a href = "hill.html?id=1" class= "choose-hill" ><img src="./public/images/arrow-white.svg"></a>')
+
+const popup3 = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Ladeira do Canteiro <a href = "hill.html?id=1" class= "choose-hill" ><img src="./public/images/arrow-white.svg"></a>')
+
+const popup4 = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Pista do Senna <a href = "hill.html?id=1" class= "choose-hill" ><img src="./public/images/arrow-white.svg"></a>')
+
+const popup5 = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Quadras Abandonadas <a href = "hill.html?id=1" class= "choose-hill" ><img src="./public/images/arrow-white.svg"></a>')
+
+const popup6 = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Ladeira do Barro <a href = "hill.html?id=1" class= "choose-hill" ><img src="./public/images/arrow-white.svg"></a>')
+
+// create and add marker
+L
+    .marker([2.838165, -60.652934], { icon })
+    .addTo(map)
+    .bindPopup(popup1);
+
+L
+    .marker([2.838500, -60.681833], { icon })
+    .addTo(map)
+    .bindPopup(popup2);
+
+L
+    .marker([2.862928, -60.659099], { icon })
+    .addTo(map)
+    .bindPopup(popup3);
+
+L
+    .marker([2.832520, -60.685648], { icon })
+    .addTo(map)
+    .bindPopup(popup4);
+
+L
+    .marker([2.841599, -60.676589], { icon })
+    .addTo(map)
+    .bindPopup(popup5);
+
+L
+    .marker([2.863139, -60.656523], { icon })
+    .addTo(map)
+    .bindPopup(popup6);
+
